caf258441147c8961985228e70d1fb4480e9c29e
Милан Молдованов <milan.moldovanov@yahoo.com>
2024-08-16 12:30:29 +0200

    Update BaseHero.java

--- Zadatak3/src/BaseHero.java
+++ Zadatak3/src/BaseHero.java
@@ -4,25 +4,20 @@ public abstract class BaseHero {
     public boolean isDead;
 
     //metoda za opis ponašanja kada heroj biva napadnut i trpi štetu koja je raspoređena na određenoj površini
-    //method that describes what happens when a hero is under attack with area of effect
     public void areaOfEffect(){
         receiveHit();
     }
 
-    //abstraktna metoda za opis ponašanja kada heroj biva napadnut i trpi štetu
-    //abstract method that describes what happens when a hero is under attack
+    //apstraktna metoda za opis ponašanja kada heroj biva napadnut i trpi štetu
     public abstract void receiveHit();
 
-    //abstraktna metoda za opis ponašanja kada heroj napada primarnim oružjem ili veštinom
-    //abstract method that describes the primary weapon attack
+    //apstraktna metoda za opis ponašanja kada heroj napada primarnim oružjem ili veštinom
     public abstract void primaryFire();
 
-    //abstraktna metoda za opis ponašanja kada heroj napada sekindarnim oružjem ili veštinom
-    //abstract method that describes the secondary weapon attack
+    //apstraktna metoda za opis ponašanja kada heroj napada sekindarnim oružjem ili veštinom
     public abstract void secondaryFire();
 
-    //abstraktna metoda koja ispisuje trenutne parametre heroja
-    //abstract method that displays the hero stats
+    //apstraktna metoda koja ispisuje trenutne parametre heroja
     public abstract void show();
 
 
